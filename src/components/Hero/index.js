e722aaf2e60fbb6a2b50543d95a9dc4cb0565dde
marcelo <madeinhellh@gmail.com>
2021-04-25 03:10:51 -0800

    Sidebar dos

--- src/components/Hero/index.js
+++ src/components/Hero/index.js
@@ -1,11 +1,26 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Navbar from '../Navbar';
-import { HeroContainer, HeroContent, HeroItems, HeroH1, HeroP, HeroBtn } from './HeroElements';
+import Sidebar from '../Sidebar'
+import {
+     HeroContainer,
+     HeroContent,
+     HeroItems,
+     HeroH1,
+     HeroP,
+     HeroBtn 
+    } from './HeroElements';
 
 const Hero = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggle = () => {
+      setIsOpen(!isOpen);
+  };
+
     return (
         <HeroContainer>
-            <Navbar />
+            <Navbar toggle={toggle} />
+            <Sidebar isOpen={isOpen} toggle={toggle} />
             <HeroContent>
               <HeroItems>
               <HeroH1>Greatest Pizza Ever</HeroH1>
---
+++ src/components/Sidebar/index.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import {
+    SidebarContainer,
+    Icon,
+    CloseIcon,
+    SidebarMenu,
+    SidebarLink,
+    SidebarRoute,
+    SidebarWrap
+} from './SidebarElements';
+
+const Sidebar = ({isOpen, toggle}) => {
+    return (
+        <SidebarContainer isOpen={isOpen} onClick={toggle}>
+            <Icon>
+                <CloseIcon />
+            </Icon>
+            <SidebarMenu>
+                <SidebarLink to='/'>Pizzas</SidebarLink>
+                <SidebarLink to='/'>Desserts</SidebarLink>
+                <SidebarLink to='/'>Full Menu</SidebarLink>
+            </SidebarMenu>
+            <SidebarWrap>
+                <SidebarRoute to='/'>Order Now</SidebarRoute>
+            </SidebarWrap>
+        </SidebarContainer>
+    );
+};
+
+export default Sidebar
+
