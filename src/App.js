fa670566ddaef0326b320c27a60d8933f3a2e857
marcelo <madeinhellh@gmail.com>
2021-04-27 17:17:41 -0800

    creating Footer

--- src/App.js
+++ src/App.js
@@ -3,8 +3,9 @@ import { BrowserRouter as Router} from 'react-router-dom';
 import { GlobalStyle } from './golbalStyles';
 import Hero from './components/Hero';
 import Products from './components/Products';
-import { productData } from './components/Products/data';
+import { productData, productDataTwo } from './components/Products/data';
 import Feature from './components/Feature';
+import Footer from './components/Footer';
 
 function App() {
   return (
@@ -13,6 +14,8 @@ function App() {
         <Hero />
         <Products heading='Choose your favorite' data={productData} />
         <Feature />
+        <Products heading='Sweet Treats for you' data={productDataTwo} />
+        <Footer />
     </Router>
   );
 }
--- src/components/Products/data.js
+++ src/components/Products/data.js
@@ -1,6 +1,10 @@
 import product1 from '../../images/product-1.jpg';
 import product2 from '../../images/product-2.jpg';
 import product3 from '../../images/product-3.jpg';
+import sweet1 from '../../images/sweet-1.jpg';
+import sweet2 from '../../images/sweet-2.jpg';
+import sweet3 from '../../images/sweet-3.jpg';
+
 
 export const productData = [
     {
@@ -28,4 +32,32 @@ export const productData = [
         button: 'Add to Cart'
       },
       
+]
+
+export const productDataTwo = [
+  {
+    img: sweet1,
+    alt: 'Pizza',
+    name: 'Supreme Pizza',
+    desc: 'Marinara sauce, basil, italian sausage, roma tomatoes, olives and pesto',
+    price: '$19.99',
+    button: 'Add to Cart'
+  },
+  {
+      img: sweet2,
+      alt: 'Pizza',
+      name: 'Hawaiian Paradise',
+      desc: 'Marinara sauce, basil, italian sausage, roma tomatoes, olives and pesto',
+      price: '$16.99',
+      button: 'Add to Cart'
+    },
+    {
+      img: sweet3,
+      alt: 'Pizza',
+      name: 'Veggie Overload',
+      desc: 'Marinara sauce, basil, italian sausage, roma tomatoes, olives and pesto',
+      price: '$14.99',
+      button: 'Add to Cart'
+    },
+    
 ]
